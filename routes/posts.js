0cf34ac5db48023a64cdf0fd290e3121caf2383c
now-0o <dlwpdn0704@gmail.com>
2023-10-22 20:16:23 +0900

    feat: 게시글 전체 조회 기능 추가

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const { Post } = require('../models')
 
+router.get('/', async (req, res) => {
+   const post = await Post.findAll({
+    attributes: ['id', 'title', 'content']
+   });
+  res.status(200).send(post);
+})
+
 router.post('/', async (req, res) => {
     const {title, content} = req.body;
     const savedPost = await Post.create({
