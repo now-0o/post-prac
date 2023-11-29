1d33b90dbf73081e55d162d22f0c8370dad3e22e
now-0o <dlwpdn0704@gmail.com>
2023-11-29 21:02:13 +0900

    feat: 결제 model 추가

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@ const Post = require('./Post');
 const Category = require('./Category');
 const Hashtag = require('./Hashtag');
 const Comment = require('./Comment');
+const Payment = require('./Payment');
 
 Category.hasMany(Post);
 Post.belongsTo(Category);
@@ -17,5 +18,6 @@ module.exports = {
     Post,
     Category,
     Hashtag,
-    Comment
+    Comment,
+    Payment
 }
